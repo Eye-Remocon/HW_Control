ecccde3416d0ed8b46bc4771723e5de13156e517
LeeSeungHui <hithere1012@naver.com>
2021-09-25 23:38:22 +0900

    LED ON serial 8(white color)

--- led/led.py
+++ led/led.py
@@ -30,3 +30,10 @@ def off():
             val = '0'
             val = val.encode('utf-8')
             ser.write(val)  # 아두이노에 시리얼통신으로 val 값 전송
+def on():
+    ser = serial.Serial('COM5', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    while (True):
+        if ser.readable():
+            val = '8'
+            val = val.encode('utf-8')
+            ser.write(val)  # 아두이노에 시리얼통신으로 val 값 전송
