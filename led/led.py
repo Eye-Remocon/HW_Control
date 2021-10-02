b42cf1191f973cbd0bee68b445e3555bcb0cf574
LeeSeungHui <hithere1012@naver.com>
2021-10-02 21:38:26 +0900

    change led.py file

--- led/led.py
+++ led/led.py
@@ -1,11 +1,15 @@
 import serial  # 시리얼통신을 위한 pyserial import
+import time
 
 
-def color_change(emotion, power):
-    ser = serial.Serial('COM5', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
-
-    while (power):
+def color_change(emotion):
+    ser = serial.Serial('COM3', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    count = 0
+    while True:
+        if count > 2:
+            break
         if ser.readable():
+            print('color')
             if emotion == "ANGRY":
                 val = '1'
             elif emotion == "DISGUST":
@@ -21,19 +25,50 @@ def color_change(emotion, power):
             elif emotion == "SURPRISE":
                 val = '7'
             val = val.encode('utf-8')
+
+            print(val)
             ser.write(val)  # 아두이노에 시리얼통신으로 val 값 전송
+            time.sleep(0.5)
+            count += 1
+
 
 def off():
-    ser = serial.Serial('COM5', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    ser = serial.Serial('COM3', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    count = 0
     while (True):
+        if count > 2:
+            break
         if ser.readable():
+            print('off')
             val = '0'
             val = val.encode('utf-8')
             ser.write(val)  # 아두이노에 시리얼통신으로 val 값 전송
+            time.sleep(0.5)
+            count += 1
 def on():
-    ser = serial.Serial('COM5', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    ser = serial.Serial('COM3', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    count = 0
     while (True):
+        if count > 2:
+            break
         if ser.readable():
+            print('on')
             val = '8'
             val = val.encode('utf-8')
             ser.write(val)  # 아두이노에 시리얼통신으로 val 값 전송
+            time.sleep(0.5)
+            count += 1
+
+def emergency():
+    ser = serial.Serial('COM3', 9600)  # 아두이노와 시리얼통신을 위한 포트 연결 및 보드레이트 일치시킴
+    count = 0
+    while (True):
+        if count > 2:
+            break
+        if ser.readable():
+            print('on')
+            val = '9'
+            val = val.encode('utf-8')
+            ser.write(val)  # 아두이노에 시리얼통신으로 val 값 전송
+            time.sleep(0.5)
+            count += 1
